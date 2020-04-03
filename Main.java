33547ef11b9e69f7e803e2eb616ec757cf4a053e
ddd1999 <61756600+ddd1999@users.noreply.github.com>
2020-04-03 19:49:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,8 +15,8 @@ public class Main {
         Player player2 = new Player("PLAYER 2");
         Player player3 = new Player("PLAYER 3");
 
-        Board tabla = new Board(Arrays.asList(0,0, 1, 2, 3, 4, 5, 6, 7, 8));
-        Game game = new Game(10, tabla, Arrays.asList(player1, player2, player3));
+        Board board = new Board(Arrays.asList(0,0, 1, 2, 3, 4, 5, 6, 7, 8));
+        Game game = new Game(5, board, Arrays.asList(player1, player2, player3));
         game.startGame();
     }
 }
